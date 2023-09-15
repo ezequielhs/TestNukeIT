1b4019f53022e6bdd2c40720bcb87ac82912d8af
ezequielhs <ezequielhoyos@outlook.com>
2023-09-15 20:31:02 -0300

    feat(Main): Change title v2

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <main className="h-screen w-screen flex items-center justify-center flex-col gap-8">
       <div className="flex items-center flex-col">
-        <H1 className="text-8xl">Hello World</H1>
+        <H1 className="text-4xl">Hello World</H1>
         <H2>Destroy complexity, create beauty.</H2>
       </div>
 
